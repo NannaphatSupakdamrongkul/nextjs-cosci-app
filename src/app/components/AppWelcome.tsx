0f542670eb67d0e80a7b5d53c162e7801d650963
nannaphat <nanaphat1985@gmail.com>
2025-05-27 14:21:39 +0700

    add shadcnui

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,22 +1,29 @@
 'use client'
 
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
 type AppWelcomeProps = {
     headTitle?: string
     isShow: boolean;
 }
 
 export default function AppWelcome({headTitle,isShow}: AppWelcomeProps) {
-const title = 'Welcome to cosci';
+//let title = 'Welcome to cosci';
+const [title, setTitle] = useState('Welcome to cosci');
 const currentYear = <p>2025</p>;
-const handleclick = () => {
-    alert('Hello TypeScript')
+const handleClick = () => {
+    //title = 'Welcome to SWU';
+    setTitle('Welcome to SWU');
+    //alert('Hello TypeScript')
 }
 
     return (
         <>
             <h1>{headTitle}</h1>
             <p>{title.toUpperCase()}</p>
-            <button className="bg-blue-700 p-3 m-3 text-white rounded-lg"onClick={handleclick}>กดได้เลย!</button>
+            <Button onClick={handleClick}>Click Me</Button>
+            <button className="bg-blue-700 p-3 m-3 text-white rounded-lg"onClick={handleClick}>กดได้เลย!</button>
             {currentYear}
             {
                 isShow && <p>Date; 10/10/1998</p>
