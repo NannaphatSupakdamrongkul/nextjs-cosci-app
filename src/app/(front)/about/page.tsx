58bc0d85da2d858a5a3eefaeb61b09acb4c0d24b
nannaphat <nanaphat1985@gmail.com>
2025-05-28 11:55:21 +0700

    change nav bar

---
+++ src/app/(front)/about/page.tsx
@@ -0,0 +1,9 @@
+import Contact01Page from "@/components/contact-01/contact-01";
+
+export default function Page() {
+  return (
+    <main>
+      <Contact01Page />
+    </main>
+  );
+}
--- src/app/(front)/page.tsx
+++ src/app/(front)/page.tsx
@@ -1,5 +1,5 @@
 import Hero02 from "@/components/hero-02/hero-02";
-import AppWelcome from "./components/AppWelcome";
+
 import Features01Page from "@/components/features-01/features-01";
 import Footer03Page from "@/components/footer-03/footer-03";
 
