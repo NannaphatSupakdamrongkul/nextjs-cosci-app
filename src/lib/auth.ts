9a7f4eff4b3af27f3ad7e386674cc827cda151c8
nannaphat <nanaphat1985@gmail.com>
2025-05-30 11:08:04 +0700

    create route

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,19 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "@/db"; // your drizzle instance
+import { account, session, user } from "@/db/schema";
+import { verification } from "../../auth-schema";
  
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "mysql", // or "mysql", "sqlite"
-    })
+        schema: { user, account, session, verification },
+        usePlural: false
+    }),
+    emailAndPassword: {
+        enabled: true,
+        autoSignIn: false,
+        requireEmailVerification: false,
+        minPasswordLength: 6
+    }
 });
